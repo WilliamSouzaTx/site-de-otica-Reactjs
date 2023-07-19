b85d26c7c81566cf857671912a9d38db68bea031
WilliamSouzaTx <williamsouzatx@gmail.com>
2023-07-19 11:30:08 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
+
+import Topo from './componentes/Topo';
+import SecaoCapa from './componentes/Conteudo/SecaoCapa';
+import SecaoProdutos from './componentes/Conteudo/SecaoProdutos';
+import SecaoSobre from './componentes/Conteudo/SecaoSobre';
+import SecaoContato from './componentes/Conteudo/SecaoContato';
+import Rodape from './componentes/Rodape';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Topo />
+      <SecaoCapa/>
+      <SecaoProdutos/>
+      <SecaoSobre/>
+      <SecaoContato/>
+      <Rodape/>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,11 @@
+import './estilos.css';
+
+export default function Rodape() {
+  return (
+    <footer>
+      <div className='limitar-secao secao-rodape'>
+        <p>© 2022 Óticas Vida. - Todos os direitos reservados.</p>
+      </div>
+    </footer>
+  )
+}
